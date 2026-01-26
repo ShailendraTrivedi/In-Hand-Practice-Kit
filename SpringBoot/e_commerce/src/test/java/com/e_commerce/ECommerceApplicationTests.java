c81632a7dc4d3a7d08c5e765d89a3edf6c2a754e
Shailendra Trivedi <113613871+ShailendraTrivedi@users.noreply.github.com>
2026-01-27 03:32:34 +0530

    In-Hand-Practice-Kit

--- SpringBoot/e_commerce/src/test/java/com/e_commerce/ECommerceApplicationTests.java
+++ SpringBoot/e_commerce/src/test/java/com/e_commerce/ECommerceApplicationTests.java
@@ -1,8 +1,5 @@
 package com.e_commerce;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 // @SpringBootTest
 class ECommerceApplicationTests {
 
